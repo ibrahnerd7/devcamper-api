0d9af4530390da1cbcbe3a669b1773938a79a95a
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-27 21:48:25 +0300

    Creating controller methods

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express=require('express');
-const dotenv=require('dotenv');
+const express =require('express');
+const dotenv =require('dotenv');
+
+//Route  files
+const bootcamps=require('./routes/bootcamps');
 
 //Load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
-const app=express();
+const app = express();
 
-const PORT=process.env.PORT || 5000;
+//Mount routers 
+app.use('/api/v1/bootcamps',bootcamps);
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}!`);
