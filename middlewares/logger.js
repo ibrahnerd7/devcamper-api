5a4cc99761daa3b9260744743c96f0cc20bf5d68
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-27 22:02:17 +0300

    Intro to middleware

---
+++ middlewares/logger.js
@@ -0,0 +1,8 @@
+//@desc  Logs reqest to console
+const logger=(req,res,next)=>{
+    req.hello='Hello world';
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+module.exports=logger;
