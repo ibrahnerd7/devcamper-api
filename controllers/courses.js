c982936be5237b4d687a8c3e1b8e270a538c5e9a
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-28 18:18:29 +0300

    Update and delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -10,25 +10,25 @@ const Bootcamp = require('../models/Bootcamp');
  * @route    GET /api/v1/bootcamps/:bootcampId/courses
  * @access   Public
  */
-exports.getCourses=asyncHandler(async (req,res,next)=>{
+exports.getCourses = asyncHandler(async (req, res, next) => {
     let query;
 
-    if(req.params.bootcampId){
-        query=Course.find({bootcamp:req.params.bootcampId})
+    if (req.params.bootcampId) {
+        query = Course.find({ bootcamp: req.params.bootcampId })
     }
-    else{
-        query=Course.find().populate({
-            path:'bootcamp',
-            select:'name description'
+    else {
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
         });
     }
 
-    const courses=await query;
+    const courses = await query;
 
     res.status(200).json({
-        success:true,
-        count:courses.length,
-        data:courses
+        success: true,
+        count: courses.length,
+        data: courses
     })
 });
 
@@ -37,19 +37,19 @@ exports.getCourses=asyncHandler(async (req,res,next)=>{
  * @route    GET /api/v1/courses/:id
  * @access   Public
  */
- exports.getCourse=asyncHandler(async (req,res,next)=>{
-    const course=await Course.findById(req.params.id).populate({
-        path:'bootcamp',
-        select:'name description',
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description',
     })
 
-    if(!course){
-        return next(new ErrorResponse(`No course with the id of ${req.params.id}`),404)
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
     }
 
     res.status(200).json({
-        success:true,
-        data:course
+        success: true,
+        data: course
     })
 });
 
@@ -58,20 +58,60 @@ exports.getCourses=asyncHandler(async (req,res,next)=>{
  * @route    POST /api/v1/bootcamps/:bootcampId/courses
  * @access   Private
  */
- exports.addCourse=asyncHandler(async (req,res,next)=>{
-  req.body.bootcamp=req.params.bootcampId;
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
 
-  const bootcamp=await Bootcamp.findById(req.params.bootcampId);
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
-    if(!bootcamp){
-        return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),404)
+    if (!bootcamp) {
+        return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404)
     }
 
-    const course=await Course.create(req.body);
+    const course = await Course.create(req.body);
 
     res.status(200).json({
-        success:true,
-        data:course
+        success: true,
+        data: course
+    })
+});
+
+/**
+ * @desc     Update course
+ * @route    PUT /api/v1/bootcamps/courses/:id
+ * @access   Private
+ */
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course found with the id of ${req.params.id}`), 404)
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+});
+
+/**
+ * @desc     Delete course
+ * @route    DELETE /api/v1/bootcamps/courses/:id
+ * @access   Private
+ */
+ exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course found with the id of ${req.params.id}`), 404)
+    }
+
+    await course.remove()
+
+    res.status(200).json({
+        success: true,
+        data: {}
     })
 });
 
