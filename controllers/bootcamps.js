279734db07efe0d8097d08385935f7cf3dfa945f
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-28 11:35:04 +0300

    Create custome errorresponse class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp=require('../models/Bootcamp')
+const ErrorResponse=require('../utils/errorResponse');
 
 /**
  * @desc     Get all bootcamps
@@ -25,13 +26,13 @@ exports.getBootcamp=async(req,res,next)=>{
         const bootcamp=await Bootcamp.findById(req.params.id);
 
         if(!bootcamp){
-          return  res.status(400).json({success:false})  
+          return   next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404))
         }
         
         res.status(200).json({success:true,data:bootcamp})
     } catch (error) {
-        next(error)
-        // res.status(400).json({success:false}) 
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404))
+      
     }
 }
 
--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,5 +1,8 @@
 class ErrorResponse extends Error{
     constructor(message,statusCode){
-
+     super(message);
+     this.statusCode=statusCode;
     }
 }
+
+module.exports=ErrorResponse;
