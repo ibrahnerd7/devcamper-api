0cea68c4ee07a3eeaa8fb8c90d62aed493f63214
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-28 18:39:16 +0300

    Calculating the average course cost

--- models/Course.js
+++ models/Course.js
@@ -1,43 +1,83 @@
-const mongoose=require('mongoose');
- 
+const mongoose = require('mongoose');
+const colors = require('colors');
 
-const CourseSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        trim:true,
-        required:[true,'Please add a course title']
+
+const CourseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a course title']
     },
-    title:{
-        type:String,
-        required:[true,'Please add a description']
+    title: {
+        type: String,
+        required: [true, 'Please add a description']
     },
-    weeks:{
-        type:String,
-        required:[true,'Please add a number of weeks']
+    weeks: {
+        type: String,
+        required: [true, 'Please add a number of weeks']
     },
-    tuition:{
-        type:Number,
-        required:[true,'Please add a tuition cost']
+    tuition: {
+        type: Number,
+        required: [true, 'Please add a tuition cost']
     },
-    minimumSkill:{
-        type:String,
-        required:[true,'Please add a minumum skill'],
-        enum:['beginner','intermediate','advanced']
+    minimumSkill: {
+        type: String,
+        required: [true, 'Please add a minumum skill'],
+        enum: ['beginner', 'intermediate', 'advanced']
     },
-    scholarshipAvailable:{
-        type:Boolean,
-        default:false,
+    scholarshipAvailable: {
+        type: Boolean,
+        default: false,
     },
-    createdAt:{
-        type:Date,
-        default:Date.now
+    createdAt: {
+        type: Date,
+        default: Date.now
     },
-    bootcamp:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Bootcamp',
-        required:true,
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true,
     }
 });
 
+//Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating avg cost'.blue)
 
-module.exports=mongoose.model('Course',CourseSchema);
+    const obj = await this.aggregate([
+        {
+            $match: {
+                bootcamp: bootcampId
+            }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: {
+                    $avg: '$tuition'
+                }
+            }
+        }
+    ])
+
+   try {
+     await this.model('Bootcamp').findByIdAndUpdate(bootcampId,{
+         averageCost:Math.ceil(obj[0].averageCost /10 )* 10
+     })  
+   } catch (error) {
+       console.error(error);
+   }
+}
+
+//Call getAverageCost after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Call getAverageCost before remove
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+
+module.exports = mongoose.model('Course', CourseSchema);
