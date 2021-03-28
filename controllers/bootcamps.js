24aecb604751be8ea3297a1eadc3f41634970b15
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-28 14:01:16 +0300

    Add advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,7 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middlewares/async');
-const geocoder=require('../utils/geocoder');
+const geocoder = require('../utils/geocoder');
 const { geocode } = require('../utils/geocoder');
 
 /**
@@ -10,7 +10,15 @@ const { geocode } = require('../utils/geocoder');
  * @access   Public
  */
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
 
     res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
 
@@ -23,7 +31,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
  */
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
-
+  
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
@@ -83,30 +91,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
  * @route    GET /api/v1/bootcamps/radius/:zipcode/:distance
  * @access   Private  
  */
- exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-  const {zipcode,distance}=req.params;
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
 
-  //Get lat/lng from geocoder
-  const loc=await geocoder.geocode(zipcode);
-  const lat=loc[0].latitude;
-  const lng=loc[0].longitude;
+    //Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
 
-  //Calc raius using radians
-  //Divide distance by radius of earth
-  //Earth radius =3,963 mi / 6,378km
-  const radius=distance/3963;
+    //Calc raius using radians
+    //Divide distance by radius of earth
+    //Earth radius =3,963 mi / 6,378km
+    const radius = distance / 3963;
 
-  const bootcamps=await Bootcamp.find({
-      location:{
-          $geoWithin:{
-              $centerSphere:[[lng,lat],radius]
-          }
-      }
-  })
-res.status(200).json({
-    success:true,
-    count:bootcamps.length,
-    data:bootcamps
-})
+    const bootcamps = await Bootcamp.find({
+        location: {
+            $geoWithin: {
+                $centerSphere: [[lng, lat], radius]
+            }
+        }
+    })
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    })
 
 })
