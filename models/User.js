daff59d58542d0f27df12a0ba593f22ab6f7bd54
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 17:25:43 +0300

    User login

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -43,10 +43,15 @@ UserSchema.pre('save', async function (next) {
 });
 
 //Sign JWT and return
-UserSchema.methods.getSignedJwtToken=function(){
-    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-        expiresIn:process.env.JWT_EXPIRE
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
     })
+}
+
+//Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
 }
 
 module.exports = mongoose.model('User', UserSchema);
