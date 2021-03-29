fa2d6c1b8ed11cadf12d72b69244c77e075d39d4
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 16:56:01 +0300

    Sign & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,5 +13,8 @@ exports.register=asyncHandler(async (req,res,next)=>{
   //Create user
   const user=await User.create({name,email,password,role});
 
-  res.status(200).json({success:true})
+  //Create token
+  const token=user.getSignedJwtToken();
+
+  res.status(200).json({success:true,token,})
 })
