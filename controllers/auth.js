03c28a828b9ce05ab98d32d7312c0d37cee3ba6b
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 16:29:47 +0300

    Create user model

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middlewares/async');
+
+/**
+ * @desc     Register user
+ * @route    POST /api/v1/auth/register
+ * @access   Public
+ */
+exports.register=asyncHandler(async (req,res,next)=>{
+    res.status(200).json({success:true});
+})
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ dotenv.config({ path: './config/config.env' });
 //Route  files
 const bootcamps=require('./routes/bootcamps');
 const courses=require('./routes/courses');
+const auth=require('./routes/auth');
 
 //Connect to database
 connectDB();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname,'public')))
 //Mount routers 
 app.use('/api/v1/bootcamps',bootcamps);
 app.use('/api/v1/courses',courses);
+app.use('/api/v1/auth',auth)
 
 app.use(errorHandler)
 
