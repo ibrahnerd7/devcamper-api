76684ccb238f9c9e3f4c0582b8d6505074d9041d
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 16:43:38 +0300

    User register and encrypting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,10 @@ const asyncHandler = require('../middlewares/async');
  * @access   Public
  */
 exports.register=asyncHandler(async (req,res,next)=>{
-    res.status(200).json({success:true});
+  const {name,email,password,role}=req.body;
+
+  //Create user
+  const user=await User.create({name,email,password,role});
+
+  res.status(200).json({success:true})
 })
--- models/User.js
+++ models/User.js
@@ -1,37 +1,44 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'Please add a name']
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
     },
     email: {
         type: String,
-        required:[true,"Please add an email"],
-        unique:true,
+        required: [true, "Please add an email"],
+        unique: true,
         match: [
-          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-          'Please add a valid email'
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
         ],
-        role:{
-            type:String,
-            enum:['user','publisher'],
-            default:'user'
-        }
-      },
-      password:{
-          type:String,
-          required:[true,"Please add a password"],
-          minLength:6,
-          select:false
-      },
-      resetPasswordToken:String,
-      resetPasswordExpire:Date,
-      createdAt:{
-          type:Date,
-          default:Date.now
-      }
+
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
+//Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports=mongoose.model('User',UserSchema);
+module.exports = mongoose.model('User', UserSchema);
