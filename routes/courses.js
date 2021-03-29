b5dc597967c5b44bb24987b81b8e32b20b51ee0a
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 16:13:25 +0300

    Advanced results middleware

--- routes/courses.js
+++ routes/courses.js
@@ -7,10 +7,17 @@ const {
   deleteCourse
 } = require('../controllers/courses');
 
-const router = express.Router({mergeParams:true});
+const Course = require('../models/Course');
+const advancedResults = require('../middlewares/advancedResults');
 
-router.route('/').get(getCourses).post(addCourse);
+
+const router = express.Router({ mergeParams: true });
+
+router.route('/').get(advancedResults(Course, {
+  path: 'bootcamp',
+  select: 'name description'
+}), getCourses).post(addCourse);
 
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
-module.exports=router;
+module.exports = router;
