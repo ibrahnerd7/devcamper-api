4c10ad3bffd004ce0095da82eb7346f7f93c827f
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-29 17:37:51 +0300

    Sending JWT in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,16 +7,13 @@ const asyncHandler = require('../middlewares/async');
  * @route    POST /api/v1/auth/register
  * @access   Public
  */
-exports.register=asyncHandler(async (req,res,next)=>{
-  const {name,email,password,role}=req.body;
+exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
 
-  //Create user
-  const user=await User.create({name,email,password,role});
+    //Create user
+    const user = await User.create({ name, email, password, role });
 
-  //Create token
-  const token=user.getSignedJwtToken();
-
-  res.status(200).json({success:true,token,})
+    sendTokenResponse(user,200,res);
 });
 
 /**
@@ -24,30 +21,46 @@ exports.register=asyncHandler(async (req,res,next)=>{
  * @route    POST /api/v1/auth/login
  * @access   Public
  */
- exports.login=asyncHandler(async (req,res,next)=>{
-    const {email,password}=req.body;
-  
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
     //Validate email and password
-    if(!email || !password){
-        return next(new ErrorResponse('Please provide and email and password',400))
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide and email and password', 400))
     }
 
     //Check for  user
-    const user=await User.findOne({email}).select('+password');
+    const user = await User.findOne({ email }).select('+password');
 
-    if(!user){
-        return next(new ErrorResponse('Invalid credentials',401))
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401))
     }
 
     //Check if password matches
-    const isMatch=await user.matchPassword(password);
+    const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
-        return next(new ErrorResponse('Invalid credentials',401))
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401))
     }
 
+    sendTokenResponse(user,200,res);
+});
+
+
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+
     //Create token
-    const token=user.getSignedJwtToken();
-  
-    res.status(200).json({success:true,token,})
-  })
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    };
+
+    if(process.env.NODE_ENV==="production"){
+        options.secure=true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({ success: true, token });
+}
