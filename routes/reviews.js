50e994398a688f09bccf604179aa687d144c766d
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-30 12:11:04 +0300

    Get single review and update seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
   getReviews,
+  getReview,
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -15,5 +16,7 @@ router.route('/').get(advancedResults(Review, {
   path: 'bootcamp',
   select: 'name description'
 }), getReviews);
+
+router.route('/:id').get(getReview)
 
 module.exports = router;
