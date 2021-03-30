7f89abe7c106e2f5bbc4cea0577fbacc0908ef0f
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-30 15:54:05 +0300

    Rate limiting, hpp and CORS

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const path=require('path');
-const express =require('express');
-const dotenv =require('dotenv');
-const morgan=require('morgan');
-const colors=require('colors');
-const mongoSanitize=require('express-mongo-sanitize');
-const helmet=require('helmet');
-const xssClean=require('xss-clean');
-const fileupload=require('express-fileupload');
-const cookieParser=require('cookie-parser');
-const errorHandler=require('./middlewares/error');
-const connectDB=require('./config/db');
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middlewares/error');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 //Route  files
-const bootcamps=require('./routes/bootcamps');
-const courses=require('./routes/courses');
-const auth=require('./routes/auth');
-const users=require('./routes/users');
-const reviews=require('./routes/reviews');
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 
 //Connect to database
@@ -34,7 +37,7 @@ app.use(express.json())
 app.use(cookieParser());
 
 //Dev logging middleware
-if(process.env.NODE_ENV=='development'){
+if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'))
 }
 
@@ -50,28 +53,42 @@ app.use(helmet());
 //Prevent XSS atacks
 app.user(xssClean());
 
+//Rate Limit
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,// 10ms
+    max: 1
+});
+
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
+
+//Enable cors
+app.use(cors());
+
 //Set static folder
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Mount routers 
-app.use('/api/v1/bootcamps',bootcamps);
-app.use('/api/v1/courses',courses);
-app.use('/api/v1/auth',auth)
-app.use('/api/v1/users',users)
-app.use('/api/v1/reviews',reviews)
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+app.use('/api/v1/re views', reviews)
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
-const server=app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold);
 });
 
 //Handle unhandled promise rejections
-process.on('unhandledRejection',(err,promise)=>{
+process.on('unhandledRejection', (err, promise) => {
     console.log(`Error:${err.message}`)
 
     //close server and exit process
-    server.close(()=>process.exit(1))
+    server.close(() => process.exit(1))
 })
