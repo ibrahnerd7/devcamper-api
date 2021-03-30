c5eb4c01fd9e8c6d707e95b10a0f1907012bb02e
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-30 10:35:04 +0300

    Update user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,19 @@
 const express=require('express');
-const {register,login, getMe, forgotPassword,resetPassword}=require('../controllers/auth');
+const {register,login, getMe, forgotPassword,resetPassword, updateDetails, updatePassword}=require('../controllers/auth');
 
 const router=express.Router();
 
 const {protect}=require('../middlewares/auth');
+const { update } = require('../models/User');
 
 router.post('/register',register);
 router.post('/login',login);
 router.get('/me',protect,getMe);
 router.post('/forgotpassword',forgotPassword);
 router.put('/resetpassword/:resetToken',resetPassword)
+router.put('/updatedetails',protect,updateDetails);
+router.put('/updatepassword',protect,updatePassword);
+
 
 
 module.exports=router;
